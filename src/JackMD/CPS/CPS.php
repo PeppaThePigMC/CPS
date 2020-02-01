19bb2a4470a72284e4100e8958f3932c1b2cdc5b
ffsjkorn <jkorn2324swagg@gmail.com>
2020-01-31 18:44:00 -0800

    Updated Jack's CPS Plugin.

--- src/JackMD/CPS/CPS.php
+++ src/JackMD/CPS/CPS.php
@@ -3,50 +3,185 @@ declare(strict_types = 1);
 
 namespace JackMD\CPS;
 
+use pocketmine\level\Position;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use pocketmine\network\mcpe\protocol\PlayerActionPacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class CPS extends PluginBase{
+
+    const SWISH_SOUNDS = [
+        LevelSoundEventPacket::SOUND_ATTACK,
+        LevelSoundEventPacket::SOUND_ATTACK_NODAMAGE,
+        LevelSoundEventPacket::SOUND_ATTACK_STRONG
+    ];
 	
 	/** @var array */
-	private $clicks;
+	private $clicks = [];
+
+	/** @var array */
+	private $deviceInput = [];
+
+	/** @var array */
+	private $actions = [];
 	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 		$this->getLogger()->info("CPS Plugin Enabled.");
 	}
+
+    /**
+     * @param string $player
+     * @param int $input
+     *
+     * Sets the player's device input.
+     */
+    public function setDeviceInput(string $player, int $input) : void {
+        $this->deviceInput[$player] = $input;
+    }
+
+    /**
+     * @param Player $player
+     * @return int
+     *
+     * Gets the player's device input.
+     */
+    public function getDeviceInput(Player $player) : int {
+        if(!isset($this->deviceInput[$player->getLowerCaseName()])) {
+            return -1;
+        }
+        return $this->deviceInput[$player->getLowerCaseName()];
+    }
+
 	
 	/**
 	 * @param Player $player
 	 * @return int
 	 */
-	public function getClicks(Player $player): int{
-		if(!isset($this->clicks[$player->getLowerCaseName()])){
-			return 0;
-		}
-		$time = $this->clicks[$player->getLowerCaseName()][0];
-		$clicks = $this->clicks[$player->getLowerCaseName()][1];
-		if($time !== time()){
-			unset($this->clicks[$player->getLowerCaseName()]);
-			return 0;
-		}
-		return $clicks;
+	public function getClicks(Player $player): int {
+
+		$clicks = $this->updateClicks($player);
+
+		return count($clicks);
 	}
-	
-	/**
-	 * @param Player $player
-	 */
-	public function addClick(Player $player): void{
-		if(!isset($this->clicks[$player->getLowerCaseName()])){
-			$this->clicks[$player->getLowerCaseName()] = [time(), 0];
-		}
-		$time = $this->clicks[$player->getLowerCaseName()][0];
-		$clicks = $this->clicks[$player->getLowerCaseName()][1];
-		if($time !== time()){
-			$time = time();
-			$clicks = 0;
-		}
-		$clicks++;
-		$this->clicks[$player->getLowerCaseName()] = [$time, $clicks];
+
+
+    /**
+     * @param Player $player
+     * @param Position|null $pos
+     * @param int
+     *
+     * Adds a click to the player.
+     */
+    public function addClick(Player $player, Position $pos = null): void{
+
+	    if(!isset($this->clicks[$player->getLowerCaseName()])){
+	        $this->clicks[$player->getLowerCaseName()] = [];
+        }
+
+	    $time = (int)round(microtime(true) * 1000);
+	    $clicks = $this->updateClicks($player, false);
+
+	    if($pos !== null) {
+	        $actions = $this->getAction($player);
+	        $lastAction = (int)$actions['previous']['action']; $lastActionTime = $actions['previous']['time'];
+	        $currentActionTime = $actions['current']['time'];
+	        if($lastAction === PlayerActionPacket::ACTION_ABORT_BREAK) {
+	            $difference = $currentActionTime - $lastActionTime;
+	            if($difference > 5) {
+	                $clicks[$time] = true;
+	                $this->clicks[$player->getLowerCaseName()] = $clicks;
+                }
+            }
+	        return;
+        }
+
+	    $clicks[$time] = true;
+	    $this->clicks[$player->getLowerCaseName()] = $clicks;
 	}
+
+    /**
+     * @param Player $player
+     * @param bool $update
+     * @return array
+     *
+     * Updates the clicks.
+     */
+    public function updateClicks(Player $player, bool $update = true) : array {
+
+	    if(!isset($this->clicks[$player->getLowerCaseName()])) {
+	        return [];
+        }
+
+        $clicks = $this->clicks[$player->getLowerCaseName()];
+        $removedClicks = [];
+
+        $currentMilliseconds = (int)round(microtime(true) * 1000);
+
+        foreach($clicks as $millis => $value) {
+            $difference = $currentMilliseconds - $millis;
+            if($difference >= 1000) {
+                $removedClicks[$millis] = true;
+            }
+        }
+
+        $clicks = array_diff_key($clicks, $removedClicks);
+        if($update) {
+            $this->clicks[$player->getLowerCaseName()] = $clicks;
+        }
+
+        return $clicks;
+    }
+
+
+    /**
+     * @param Player $player
+     * @param int $action
+     * @param Position $pos
+     *
+     * Sets the action of the player.
+     */
+    public function setAction(Player $player, int $action) : void {
+
+        $time = round(microtime(true) * 1000);
+
+        if(!isset($this->actions[$player->getLowerCaseName()])) {
+
+            $this->actions[$player->getLowerCaseName()] = [
+                'previous' => [
+                    'action' => $action,
+                    'time' => $time
+                ],
+                'current' => [
+                    'action' => $action,
+                    'time' => $time
+                ]
+            ];
+
+        } else {
+
+            $current = $this->actions[$player->getLowerCaseName()]['current'];
+
+            $this->actions[$player->getLowerCaseName()]['previous'] = $current;
+            $this->actions[$player->getLowerCaseName()]['current'] = [
+                'action' => $action,
+                'time' => $time
+            ];
+        }
+    }
+
+
+    /**
+     * @param Player $player
+     * @return array
+     *
+     * Gets the actions of the player.
+     */
+    private function getAction(Player $player) {
+        if(!isset($this->actions[$player->getLowerCaseName()])) {
+            return [];
+        }
+        return $this->actions[$player->getLowerCaseName()];
+    }
 }
--- src/JackMD/CPS/EventListener.php
+++ src/JackMD/CPS/EventListener.php
@@ -4,8 +4,15 @@ declare(strict_types = 1);
 namespace JackMD\CPS;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\level\Position;
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\protocol\PlayerActionPacket;
+use pocketmine\network\mcpe\protocol\types\InputMode;
+use pocketmine\network\mcpe\protocol\types\PlayMode;
 
 class EventListener implements Listener{
 	
@@ -25,13 +32,48 @@ class EventListener implements Listener{
 	 * @param DataPacketReceiveEvent $event
 	 */
 	public function onDataPacketReceive(DataPacketReceiveEvent $event){
-		$player = $event->getPlayer();
+
+	    $player = $event->getPlayer();
 		$packet = $event->getPacket();
-		if($packet instanceof InventoryTransactionPacket){
-			$transactionType = $packet->transactionType;
-			if($transactionType === InventoryTransactionPacket::TYPE_USE_ITEM || $transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY){
-				$this->plugin->addClick($player);
-			}
-		}
+
+		if($packet instanceof LoginPacket) {
+
+		    $name = $player->getLowerCaseName();
+		    $input = (int)$packet->clientData["CurrentInputMode"];
+		    $this->plugin->setDeviceInput($name, $input);
+
+		} elseif ($packet instanceof LevelSoundEventPacket) {
+		    $sound = $packet->sound;
+		    if(in_array($sound, CPS::SWISH_SOUNDS)) {
+		        $this->plugin->addClick($player);
+            }
+
+		} elseif ($packet instanceof PlayerActionPacket) {
+
+		    $position = new Position($packet->x, $packet->y, $packet->z, $player->getLevel());
+		    $device = $this->plugin->getDeviceInput($player);
+		    $this->plugin->setAction($player, $packet->action);
+
+		    if($packet->action === PlayerActionPacket::ACTION_START_BREAK and ($device === InputMode::MOUSE_KEYBOARD || $device === InputMode::GAME_PAD)) {
+		        $this->plugin->addClick($player, $position);
+            }
+        }
 	}
+
+
+    /**
+     * @param PlayerInteractEvent $event
+     */
+    public function onInteract(PlayerInteractEvent $event) : void {
+
+        $player = $event->getPlayer();
+
+        $action = $event->getAction();
+
+        $input = $this->plugin->getDeviceInput($player);
+
+        if($action === PlayerInteractEvent::RIGHT_CLICK_BLOCK and ($input === InputMode::TOUCHSCREEN)) {
+            $this->plugin->addClick($player);
+        }
+    }
 }
